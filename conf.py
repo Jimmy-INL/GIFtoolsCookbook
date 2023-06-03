0b7d1dcbdd746720b8b03fc4955d9bd4175c28b8
dccowan <devinccowan@gmail.com>
2023-06-02 18:41:06 -0700

    syntax bugs?

--- conf.py
+++ conf.py
@@ -64,8 +64,8 @@ linkcheck_ignore = [
   'https://e3dmt.readthedocs.io/en/latest/content/files/preFile.html#version-2-2017',
   'https://dcip2d.readthedocs.io/en/latest/content/backgroundtheory.html#equation-intMOF',
   'http://dcip2d.readthedocs.io/en/latest/content/backgroundtheory.html#equation-intMOF']
-linkcheck_retries = 3
-linkcheck_timeout = 500
+linkcheck_retries = 4
+linkcheck_timeout = 800
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
